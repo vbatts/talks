68f582d393ebfbcf49e9974b7c4dea7eb166926d
Vincent Batts <vbatts@hashbangbash.com>
2016-12-04 18:40:58 -0500

    2016-12: brush up this talk for an intro

---
+++ 2016/12-intro-to-golang/README.md
@@ -0,0 +1,6 @@
+Usage
+====
+
+	go get golang.org/x/tools/cmd/present
+	present .
+
---
+++ 2016/12-intro-to-golang/hello.go
@@ -0,0 +1,10 @@
+// +build ignore
+
+// START1 OMIT
+package main
+
+func main() {
+	println("Howdy y'all")
+}
+
+// STOP1 OMIT
